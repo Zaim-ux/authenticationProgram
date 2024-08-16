5094374a03f0bb7af2d2db82cfece3eadec4cab5
Zaim <Zaim.Abdin.2022@live.rhul.ac.uk>
2024-08-16 22:03:04 +0100

    Labels added to GUI to show needed conditions and a submit button

--- passwordCheck.py
+++ passwordCheck.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageTk
 import os
 import re
 
+def submit():
+    password = entry.get()
+
+
 #conditions made to see if inputted password meets requirements
 lowercaseCheck = r'^(?=.*[a-z])'
 uppercaseCheck = r'^(?=.*[A-Z])'
@@ -15,6 +19,8 @@ passwordVerification = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])'
 
 #Retrieves the file path for the image needed
 script_dir = os.path.dirname(__file__)
+
+
 
 #window is created and icon is customised 
 window = Tk()
@@ -30,6 +36,11 @@ entry = Entry()
 entry.config(font= 25)
 entry.pack()
 
+#Submit button created on GUI which will call the submit() method
+#allows user to enter their chosen password to be verified
+submit = Button(window, text="submit", command=submit, padx=30, pady=20, font=20)
+submit.pack(side = BOTTOM)
+
 #Green tick image created to indicate a condition has been met on GUI
 greenPath = os.path.join(script_dir, 'greenTick.png')
 greenTick = Image.open(greenPath).resize((20, 20))
@@ -39,5 +50,23 @@ green = ImageTk.PhotoImage(greenTick)
 redPath = os.path.join(script_dir, 'redX.png')
 redX = Image.open(redPath).resize((20, 20))
 red = ImageTk.PhotoImage(redX)
+
+#Labels created to show the user which conditions are required for their password as well as
+#if the condition has been met with an image
+condition1 = Label(window, text="8 characters long?", image=red, compound='right')
+condition1.pack(anchor=W, padx=150)
+
+condition2 = Label(window, text="lowercase letters?", image=red, compound='right')
+condition2.pack(anchor=W, padx=150)
+
+condition3 = Label(window, text="uppercase?", image = red, compound='right')
+condition3.pack(anchor=W, padx=150)
+
+condition4 = Label(window, text="numbers?", image = red, compound='right')
+condition4.pack(anchor=W, padx=150)
+
+condition5 = Label(window, text="special characters?", image = red, compound='right')
+condition5.pack(anchor=W, padx=150)
+
 
 window.mainloop()
